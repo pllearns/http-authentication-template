54fc6f7bde264c951d58d5edbd1e0d2d58c95a40
pllearns <pllearns@gmail.com>
2017-07-21 13:46:13 -0700

    Add layout pug file and change view structure

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieSession({
+  name: 'session',
+  keys: 'some secret',
+  httpOnly: true,
+  maxAge: 24 * 60 * 60 * 1000
+}))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
